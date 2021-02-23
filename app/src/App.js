f84c426f40ec448579d15b689f6ea521ec309b2a
Stephen Booth <stephenboothdev@gmail.com>
2021-02-23 17:46:21 +0000

    app clean up

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
 import ExampleComponent from "./Components/ExampleComponent";
-import Feed from "./Components/Feed";
 
 function App() {
   return (
     <div className="App">
-      <Feed />
+      <ExampleComponent />
     </div>
   );
 }
