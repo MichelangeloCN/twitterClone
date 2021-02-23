61a471cdc87d13907098b3f6c4727e3033ecd8a5
Stephen Booth <stephenboothdev@gmail.com>
2021-02-23 15:34:02 +0000

    Displaying feed component in app for development

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 
 import ExampleComponent from "./Components/ExampleComponent";
+import Feed from "./Components/Feed";
 
 function App() {
   return (
     <div className="App">
-      <ExampleComponent />
+      <Feed />
     </div>
   );
 }
---
+++ app/src/Components/Tweet.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tweet = ({ tweet }) => {
+  return <div>{tweet.body}</div>;
+};
+
+export default Tweet;
