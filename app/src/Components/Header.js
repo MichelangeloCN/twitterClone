dd96540d7ec73906364f2468ad8af544f0d522d9
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-02-23 16:21:00 +0000

    Header
    #20

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.css';
+import profPic from './profilepic.png'
 
 const Header = () => {
     return (
         <div className="Header">
+            <img src={profPic} id="profilePic" />
                 <h1>Home</h1>
        </div>
     )
