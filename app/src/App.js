d15d272585d967ea2b14922660337d9a87240c71
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-02-23 15:12:17 +0000

    Header js & css

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,20 @@
+<<<<<<< Updated upstream
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">s
+=======
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+>>>>>>> Stashed changes
     </div>
   );
 }
