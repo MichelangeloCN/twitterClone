83e6cc50505f325b73db4fd89dbb41c365d44d3a
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-02-23 17:03:17 +0000

    Format document

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './Header.css';
-import profPic from './profilepic.png'
+import React from "react";
+import "./Header.css";
+import profPic from "./profilepic.png";
 
 const Header = () => {
-    return (
-        <div className="Header">
-            <img src={profPic} id="profilePic" alt="profilePic"/>
-                <h1>Home</h1>
-                <i className="ri-star-s-line"/>
-       </div>
-    )
-}
- 
+  return (
+    <div className="Header">
+      <img src={profPic} id="profilePic" alt="profilePic" />
+      <h1>Home</h1>
+      <i className="ri-star-s-line" />
+    </div>
+  );
+};
+
 export default Header;
