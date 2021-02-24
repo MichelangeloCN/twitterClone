c8584425f4fe6317be3e88369e0bc243b50a39d6
Stephen Booth <stephenboothdev@gmail.com>
2021-02-24 12:16:33 +0000

    Added structure to tweet component

--- app/src/Components/Tweet.js
+++ app/src/Components/Tweet.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import InteractionBar from "./InteractionBar";
+
 const Tweet = ({ tweet }) => {
-  return <div>{tweet.body}</div>;
+  return (
+    <div className="TweetContainer">
+      <div className="tweetHeader" />
+      <div className="profileImg" />
+      <div className="tweetTitle">
+        <span className="userName">{tweet.userName}</span>
+        <span className="handle">{tweet.handle}</span>
+        <span> - </span>
+        <span className="tweetTimestamp">{tweet.timestamp}</span>
+      </div>
+      <div className="tweetBody">{tweet.body}</div>
+      <InteractionBar />
+    </div>
+  );
 };
 
 export default Tweet;
