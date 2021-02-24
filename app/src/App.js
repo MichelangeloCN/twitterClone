8b95bdef12846bbc261b1a5388f69aaf10023e80
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-02-24 15:06:32 +0000

    Messages Tab
    Not completed

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./App.css";
 import ExampleComponent from "./Components/ExampleComponent";
-import InteractionBar from './Components/InteractionBar';
+import Header from "./Components/Header";
+import InteractionBar from "./Components/InteractionBar";
+import Messages from "./Components/Messages";
 
 function App() {
   return (
     <div className="App">
       <ExampleComponent />
+      <Header />
       <i className="ri-notification-3-line" />
       <InteractionBar />
+      <Messages />
     </div>
   );
 }
