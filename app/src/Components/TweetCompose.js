e05202522125cca59e447f4d23dccea01c48c3d6
Jim <jadavey@hotmail.com>
2021-02-24 11:13:10 +0000

    Tweet Compose
    rebase deleted the .js so thats annoying. grid is a nightmare.

--- app/src/Components/TweetCompose.js
+++ app/src/Components/TweetCompose.js
@@ -1,21 +1,40 @@
 import React from "react";
+import michelangelo from "./profilepic.png";
+import "./TweetCompose.css";
 
 const TweetCompose = () => {
   return (
     <div className="tweetCompose">
-      <image src=""></image>
+      <div className="profilePic">
+        <image src={michelangelo} />
+      </div>
+
       <div className="inputText">
-        <input type="text">"What's Happening"</input>
+        <input type="text" />
       </div>
+
       <div className="tweetComposeIcons">
-        <i class="ri-image-line"></i>
-        <i class="ri-file-gif-line"></i>
-        <i class="ri-bar-chart-2-line"></i>
-        <i class="ri-emotion-happy-line"></i>
-        <i class="ri-calendar-todo-line"></i>
+        <div className="media">
+          <i className="ri-image-line"> </i>
+        </div>
+        <div className="GIF">
+          <i className="ri-file-gif-line"> </i>
+        </div>
+        <div className="poll">
+          <i className="ri-bar-chart-2-line" />
+        </div>
+        <div className="emoji">
+          <i className="ri-emotion-happy-line"> </i>
+        </div>
+        <div className="schedule">
+          <i className="ri-calendar-todo-line"> </i>
+        </div>
       </div>
-      <div className="tweetComposeButton">
-        <i class="ri-quill-pen-fill"></i>
+
+      <div>
+        <button className="tweetComposeButton" type="submit">
+          Tweet
+        </button>
       </div>
     </div>
   );
