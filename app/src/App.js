65e0fe95c99785b18b7a5e903c8e18906e5cd392
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-02-25 09:51:15 +0000

    changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import ExampleComponent from "./Components/ExampleComponent";
 import Header from "./Components/Header";
 import InteractionBar from "./Components/InteractionBar";
 import Messages from "./Components/Messages";
@@ -9,9 +8,7 @@ import MobileNavBar from "./Components/MobileNavBar";
 function App() {
   return (
     <div className="App">
-      <ExampleComponent />
       <Header />
-      <i className="ri-notification-3-line" />
       <InteractionBar />
       <Messages />
       <MobileNavBar />
