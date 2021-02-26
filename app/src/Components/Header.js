fe4aa7298d8a2b655b3125a958b169778853b1bb
Riley <rileyxcroft@gmail.com>
2021-02-26 15:03:36 +0000

    Added Side Nav on mobile

--- app/src/Components/Header.js
+++ app/src/Components/Header.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Header.css";
 import profPic from "./profilepic.png";
+import SideNavbar from './SideNavbar'
 import ProfilePicture from "./ProfilePicture";
 
 const Header = () => {
+  
+  const [buttonState, setButtonState] = useState(true);
+  const [navState, setNavState] = useState(true);
+
   return (
     <div className="Header">
+      {buttonState ? (
+      <button
+      className="NavButton"
+      type="button"
+      onClick={() => {
+      setButtonState(!buttonState);
+      setNavState(!navState);
+      }}
+      >
       <ProfilePicture src={profPic} size="small" id="headerProfilePic"  />
+      </button>
+      ) : (
+        <SideNavbar />
+      )}
+
       <h1>Home</h1>
       <i className="ri-star-s-line" />
     </div>
